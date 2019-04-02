9e57c4466434e0eb69ee3948b47d1715f49c873c
yFGq-Ziw <rui_aso@live.jp>
2019-04-02 11:09:27 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
+
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+
+  resources :microposts, only: [:create, :destroy]
 end
