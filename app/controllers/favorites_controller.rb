1773b6058e06ce3cc8d6d104a3aed8abc6d1e4b3
yFGq-Ziw <rui_aso@live.jp>
2019-04-04 15:22:00 +0000

    shusei2

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,13 +5,13 @@ class FavoritesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.fav(micropost)
     flash[:success] = '投稿をお気に入りに追加しました。'
-    redirect_to root_url
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.unfav(micropost)
     flash[:success] = '投稿のお気に入りを解除しました。'
-    redirect_to root_url
+    redirect_back(fallback_location: root_path)
   end
 end
